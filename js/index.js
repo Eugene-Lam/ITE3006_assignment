4dc58d6380dd4d285314993d4c9af824fa5034b4
kingeugeneIV <47935524+kingeugeneIV@users.noreply.github.com>
2020-04-27 01:11:07 +0800

    Finish

--- js/index.js
+++ js/index.js
@@ -1,20 +1,33 @@
+function print_result() {
+	alert("Name : " + document.f1.name.value +
+	"\nAge : " + document.f1.age.value +
+	"\nE-mail : " + document.f1.mail.value +
+	"\nComment : " + document.f1.comments.value);
+}
+
 window.onload = function () {
     function change() {
-        const bgImg = ["./img/bg1.png", "./img/bg2.png", "./img/bg3.png"];
-        const i = Math.floor((Math.random() * 3));
+        let bgImg = ["/img/bg1.png", "/img/bg2.png", "/img/bg3.png"];
+        let i = Math.floor((Math.random() * 3));
         document.body.style.backgroundImage = 'url("' + bgImg[i] + '")';
     }
     window.setInterval(change, 5000);
-}
+};
 
 var pg = 1;
-function nextpg() {
- pg;
-    	for (let p of pages) {
-		if (pg > pages.length) {
-			pg = 1;
-		}
-        if (p.id != pg) {
-            p.
-        }
-    
+showDivs(pg);
+
+function change(n) {
+	showDivs(pg += n);
+}
+
+function showDivs(n) {
+	var i;
+	var x = document.getElementsByClassName("page");
+	if (n > x.length) {pg = 1}
+	if (n < 1) {pg = x.length}
+	for (i = 0; i < x.length; i++) {
+		x[i].style.display = "none";
+	}
+	x[pg-1].style.display = "block";
+};
